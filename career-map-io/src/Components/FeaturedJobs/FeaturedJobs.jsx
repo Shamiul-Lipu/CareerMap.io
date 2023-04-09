29791e6afa719a97fd53b52e158a18bc8adf17d7
unknown <shamiullipu@gmail.com>
2023-04-10 05:07:31 +0600

    show all button added

--- career-map-io/src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ career-map-io/src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FeaturedJob from './FeaturedJob';
 import { useLoaderData } from 'react-router-dom';
 
 const FeaturedJobs = () => {
     const featuredJobsDetail = useLoaderData();
+    const [showAll, setShowAll] = useState(false);
+    // show all jobs 
+    const handleShowAll = () => {
+        setShowAll(true)
+    }
+
     console.log(featuredJobsDetail);
     return (
         <>
-            <div className='grid grid-cols-2 gap-10'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10 py-9'>
                 {
-                    featuredJobsDetail.map(job => <FeaturedJob key={job.id} job={job} />)
+                    showAll
+                        ? featuredJobsDetail.map(job => <FeaturedJob key={job.id} job={job} />)
+                        : featuredJobsDetail.slice(0, 4).map(job => <FeaturedJob key={job.id} job={job} />)
                 }
             </div>
+            <button onClick={handleShowAll} className={showAll ? 'hidden' : 'button-primary mt-5'}>Show All Jobs</button>
         </>
     );
 };
